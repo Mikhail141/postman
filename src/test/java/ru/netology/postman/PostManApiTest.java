c800019183bd279f9eaa6f34962df6d880f69332
Mikhail141 <70147484+Mikhail141@users.noreply.github.com>
2020-11-25 16:31:18 +0300

    test failed

--- src/test/java/ru/netology/postman/PostManApiTest.java
+++ src/test/java/ru/netology/postman/PostManApiTest.java
@@ -1,21 +1,41 @@
 package ru.netology.postman;
 
+import io.restassured.specification.Argument;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class PostManApiTest {
     @Test
     void shouldReturnQueryAndGetQuery() {
-
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some data")
+                .body("HELLO")
                 .when()
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body(/* --> ваша проверка здесь <-- */)
+                .body("data", equalTo("HELLO"))
+        ;
+
+    }
+
+    @Test
+    void shouldReturnRusQueryAndGetRusQuery(){
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("привет")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("прювет"))
         ;
     }
+
+
 }
